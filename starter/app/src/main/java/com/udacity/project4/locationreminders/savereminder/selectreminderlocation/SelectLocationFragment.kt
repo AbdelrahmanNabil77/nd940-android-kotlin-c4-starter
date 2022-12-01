3f6f045e35a4c5d56a51ab1c343d21f89dfa45c4
AbdElRahman Nabil <abdelrahmann89@gmail.com>
2022-12-01 08:12:13 +0200

    added map style requested permissions

--- starter/app/src/main/java/com/udacity/project4/locationreminders/savereminder/selectreminderlocation/SelectLocationFragment.kt
+++ starter/app/src/main/java/com/udacity/project4/locationreminders/savereminder/selectreminderlocation/SelectLocationFragment.kt
@@ -2,28 +2,23 @@ package com.udacity.project4.locationreminders.savereminder.selectreminderlocati
 
 
 import android.Manifest
-import android.app.Activity
-import android.content.Intent
-import android.content.IntentSender
+import android.annotation.SuppressLint
 import android.content.pm.PackageManager
 import android.content.res.Resources
 import android.os.Bundle
 import android.util.Log
 import android.view.*
+import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
-import com.google.android.gms.common.api.ApiException
-import com.google.android.gms.common.api.ResolvableApiException
 import com.google.android.gms.location.*
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.*
-import com.google.android.material.snackbar.Snackbar
 import com.udacity.project4.R
 import com.udacity.project4.base.BaseFragment
-import com.udacity.project4.base.NavigationCommand
 import com.udacity.project4.databinding.FragmentSelectLocationBinding
 import com.udacity.project4.locationreminders.savereminder.SaveReminderViewModel
 import com.udacity.project4.utils.setDisplayHomeAsUpEnabled
@@ -31,8 +26,14 @@ import org.koin.android.ext.android.inject
 import java.util.*
 
 class SelectLocationFragment : BaseFragment(), OnMapReadyCallback {
+    val TAG = "SelectLocationFragment"
     private lateinit var map: GoogleMap
-    private val REQUEST_LOCATION_PERMISSION = 1
+    private val runningQOrLater =
+        android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q
+    private val REQUEST_FINE_AND_BACKGROUND_LOCATION_PERMISSION = 1
+    private val REQUEST_FINE_LOCATION_PERMISSION = 2
+    private val REQUEST_BACKGROUND_LOCATION_PERMISSION = 3
+
     //Use Koin to get the view model of the SaveReminder
     override val _viewModel: SaveReminderViewModel by inject()
     private lateinit var binding: FragmentSelectLocationBinding
@@ -100,9 +101,8 @@ class SelectLocationFragment : BaseFragment(), OnMapReadyCallback {
 
         map = googleMap
 
-        // Add a marker in Sydney and move the camera
         val home = LatLng(30.588003, 31.498624)
-        val zoom=18f
+        val zoom = 18f
         val snippet = String.format(
             Locale.getDefault(),
             "هنا يرقد خطورة",
@@ -110,11 +110,94 @@ class SelectLocationFragment : BaseFragment(), OnMapReadyCallback {
             31.498624
         )
 
-        map.addMarker(MarkerOptions().position(home).title("منزل خاطر").snippet(snippet)
-            .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE)))
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(home,zoom))
+        map.addMarker(
+            MarkerOptions().position(home).title("منزل خاطر").snippet(snippet)
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE))
+        )
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(home, zoom))
+        setMapStyle(map)
+        enableMyLocation()
 
     }
 
+    private fun setMapStyle(map: GoogleMap) {
+        try {
+            // Customize the styling of the base map using a JSON object defined
+            // in a raw resource file.
+            val success = map.setMapStyle(
+                MapStyleOptions.loadRawResourceStyle(
+                    requireContext(),
+                    R.raw.map_style
+                )
+            )
 
+            if (!success) {
+                Log.e(TAG, "Style parsing failed.")
+            }
+        } catch (e: Resources.NotFoundException) {
+            Log.e(TAG, "Can't find style. Error: ", e)
+        }
+    }
+
+    private fun isFineLocationPermissionGranted(): Boolean {
+        return ContextCompat.checkSelfPermission(
+            requireContext(),
+            Manifest.permission.ACCESS_FINE_LOCATION
+        ) === PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun isBackgroundLocationPermissionGranted(): Boolean {
+        return ContextCompat.checkSelfPermission(
+            requireContext(),
+            Manifest.permission.ACCESS_BACKGROUND_LOCATION
+        ) === PackageManager.PERMISSION_GRANTED
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun enableMyLocation() {
+        if (isFineLocationPermissionGranted()) {
+            map.setMyLocationEnabled(true)
+        } else {
+            requestFineLocationPermission()
+        }
+    }
+
+    private fun requestFineLocationPermission() {
+        requestPermissions(
+            arrayOf<String>(Manifest.permission.ACCESS_FINE_LOCATION),
+            REQUEST_FINE_LOCATION_PERMISSION
+        )
+    }
+
+    private fun requestBackgroundLocationPermission() {
+        requestPermissions(
+            arrayOf<String>(Manifest.permission.ACCESS_BACKGROUND_LOCATION),
+            REQUEST_BACKGROUND_LOCATION_PERMISSION
+        )
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        // Check if location permissions are granted and if so enable the
+        // location data layer.
+        if (requestCode == REQUEST_FINE_LOCATION_PERMISSION) {
+            if (grantResults.size > 0 && (grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                if (runningQOrLater) {
+                    requestBackgroundLocationPermission()
+                } else {
+                    enableMyLocation()
+                }
+            }
+        }
+        if (requestCode == REQUEST_BACKGROUND_LOCATION_PERMISSION) {
+            if (grantResults.size > 0 && (grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                enableMyLocation()
+            } else {
+                Toast.makeText(requireContext(),getString(R.string.background_permission_denied_explanation),Toast.LENGTH_LONG).show()
+            }
+        }
+    }
 }
